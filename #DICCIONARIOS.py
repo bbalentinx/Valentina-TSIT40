8b848bd299d25fed4b3c892d1b1d2fab5983222a
bbalentinx <garnesking@gmail.com>
2023-08-14 13:56:27 -0300

    TUPLAS, LISTAS Y DICCIONARIOS

---
+++ #DICCIONARIOS.py
@@ -0,0 +1,15 @@
+#DICCIONARIOS
+
+Familiares= {18594799: "Monica", 46032044: "Angeles", 36598245: "Natalia", 40325544: "Camila"}
+
+print(Familiares[46032044])
+
+Familiares[3265879] = "Rosa"
+Familiares[34897562] = "Santiago"
+Familiares[32411233] = "Carla"
+
+print(Familiares)
+
+Numeros = {"Ramiro": 3513318283, "Martin": 3516530277, "Lautaro": 3516577109, "Sofia": 3516644604}
+
+print(Numeros)
---
+++ #TUPLAS.py
@@ -0,0 +1,13 @@
+#TUPLAS
+
+DiscografiaDuki= ("Super Sangre Joven", "Desde el Fin del Mundo", "Antes de Ameri")
+
+DiscografiaYSYA= ("Mordiendo el Bozal", "Trap de Verdad", "Hecho a Mano", "Antezana 247", "YSYSMO")
+
+DiscografiaLitKillah= ("MAWZ", "SnipeZ")
+
+discos= ["DiscografiaDuki", "DiscografiaYSYA", "DiscografiaLitKillah"]
+
+print(discos)
+
+print(DiscografiaDuki)
